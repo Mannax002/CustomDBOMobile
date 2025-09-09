2b81c05d2fd85b0c731c34de8319049208fd71c0
Guilherme Rodrigo <145240089+Mannax002@users.noreply.github.com>
2025-09-09 13:27:59 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,37 @@
 
+local nextTransformationLevel;
+local blockTransform;
+local waitingMsg;
+
+
+
+
+macro(200, function()
+	if (not hasProtections()) then return; end
+	if (blockTransform) then return; end
+	
+	
+	local level = player:getLevel();
+	if (not nextTransformationLevel or nextTransformationLevel <= level) then
+		NPC.say("!transformar");
+	end
+	
+end)
+
+
+
+onTextMessage(function(mode, text)
+	if (text:find("se transformar")) then
+		blockTransform = true;
+	elseif (text:find("Voc� precisa estar no level")) then
+		local level = text:match("%d+");
+		level = tonumber(level);
+		if (level) then
+			waitingMsg = nil;
+			nextTransformationLevel = level;
+		end
+	elseif (text == "Voc� se transformou!") then
+		waitingMsg = nil;
+		blockTransform = nil;
+	end
+end)
